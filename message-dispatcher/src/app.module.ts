40a73e6dc14552526ab7893220387916ad45c403
wendryo <wendryo.sales@gmail.com>
2025-06-02 17:06:58 -0300

    refactor: Remove AppController and AppService; update AppModule to include new modules and configure Swagger

--- message-dispatcher/src/app.module.ts
+++ message-dispatcher/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './infra/database/database.module';
+import { HttpModule } from './infra/http/http.module';
+import { MessagingModule } from './infra/messaging/messaging.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    HttpModule,
+    DatabaseModule,
+    MessagingModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- message-dispatcher/src/main.ts
+++ message-dispatcher/src/main.ts
@@ -1,8 +1,44 @@
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+async function setupSwagger(app: INestApplication) {
+  const options = new DocumentBuilder()
+    .setTitle('Message Dispatcher API')
+    .setDescription(
+      'API for Message Dispatcher. This API is used to manage messages for Message Dispatcher.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document, {
+    jsonDocumentUrl: '/api-json',
+    swaggerOptions: {
+      filter: true,
+    },
+  });
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('bootstrap');
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const PORT = app.get(ConfigService).get('PORT');
+
+  // https://docs.nestjs.com/pipes#global-scoped-pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  await setupSwagger(app);
+  await app.listen(PORT);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
